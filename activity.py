b13ffab9bc9f3fb6be04ef97d97564e6216b5ae4
Jacob <80932814+ultrajacobboy@users.noreply.github.com>
2021-06-13 13:09:49 -0700

    back, style and ascii art

--- activity.py
+++ activity.py
@@ -1,11 +1,17 @@
-from os.path import abspath, dirname
-import json
-from datetime import datetime
-import time
-from pushover import init, Client
-import requests
-import platform
-from colorama import Fore, Back, Style
+try:
+    from os.path import abspath, dirname
+    import json
+    from datetime import datetime
+    import time
+    from pushover import init, Client
+    import requests
+    import platform
+    from colorama import Fore, Back, Style
+    import sys
+    import os
+except:
+    print("Please type this: pip3 install python-pushover, colorama")
+    sys.exit()
 
 #get file path
 script = dirname(abspath(__file__))
@@ -13,17 +19,13 @@ script = dirname(abspath(__file__))
 user_os = platform.system()
 if user_os == "Windows":
     path = "\\"
+    clear = "cls"
 else:
     path = "/"
+    clear = "clear"
 
 with open(f'{script}{path}activities.json', "r") as f:
         acts = json.load(f)
-        f.close()
-
-with open(f'{script}{path}settings.json', "r") as f:
-        setting = json.load(f)
-        color = getattr(Fore, setting["color"])
-        print(color, "act-tracker")
         f.close()
 
 with open(f'{script}{path}config.json', "r") as f:
@@ -36,16 +38,17 @@ with open(f'{script}{path}config.json', "r") as f:
 
 client = Client(user, api_token=api)
 
-#activity class
 class Activity:
     def __init__(self):
         self.acts = acts
+        self.user_os = user_os
         self.path = path
         self.user = user
         self.api = api
         self.client = client
         self.discord_hook = discord_hook
         self.choose = choose
+        self.script = script
 
     def get_name(self):
         self.name = input("What is your name?\n> ")
@@ -161,19 +164,94 @@ class Activity:
         else:
             print("Invalid color")
 
+    def set_back(self):
+        new_back = input("What background color would you like? (black, red, green, yellow, blue, magenta, cyan and white)")
+        new_back = new_back.upper()
+        with open(f'{script}{path}settings.json', "r") as f:
+            current_settings = json.load(f)
+            f.close()
+        if new_back == "BLACK" or new_back == "RED" or new_back == "GREEN" or new_back == "YELLOW" or new_back == "BLUE" or new_back == "MAGENTA" or new_back == "CYAN" or new_back == "WHITE":
+            current_settings["back"] = new_back
+            with open(f'{script}{path}settings.json', "w") as f:
+                json.dump(current_settings, f)
+                f.close()
+            back = getattr(Back, current_settings["back"])
+            print(back, f"Successfully updated back to {new_back}.")
+        else:
+            print("Invalid color")
+
+    def set_style(self):
+        new_style = input("What style would you like? (Normal, dim, bright)")
+        new_style = new_style.upper()
+        with open(f'{script}{path}settings.json', "r") as f:
+            current_settings = json.load(f)
+            f.close()
+        if new_style == "NORMAL" or new_style == "DIM" or new_style == "BRIGHT":
+            current_settings["style"] = new_style
+            with open(f'{script}{path}settings.json', "w") as f:
+                json.dump(current_settings, f)
+                f.close()
+            style = getattr(Style, current_settings["style"])
+            print(style, f"Successfully updated style to {new_style}")
+
+    def clear_on_start(self):
+        with open(f'{script}{path}settings.json', "r") as f:
+            current_settings = json.load(f)
+            f.close()
+        if current_settings["clear"]:
+            current_settings["clear"] = False
+            print("Ok, the screen will no longer clear on start")
+        else:
+            current_settings["clear"] = True
+            print("Ok, the screen will clear on start")
+        with open(f'{script}{path}settings.json', "w") as f:
+            json.dump(current_settings, f)
+            f.close()
+
     def get_time(self):
         date_time = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
         print(f"It is currently {date_time}")
 
     def greeting(self):
+        with open(f'{script}{path}settings.json', "r") as f:
+            current_settings = json.load(f)
+            f.close()
+        if current_settings["clear"]:
+            os.system(clear)
+        with open(f'{script}{self.path}activities.json') as f:
+            lists = json.load(f)
+            listed = str(lists)
+            list_strip = listed.strip("\{\}")
+            f.close()
         with open(f'{script}{self.path}name.json') as f:
             names = json.load(f)
             f.close()
         name = names.get("name")
         date_time = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+        with open(f'{script}{path}settings.json', "r") as f:
+            setting = json.load(f)
+            color = getattr(Fore, setting["color"])
+            back = getattr(Back, setting["back"])
+            style = getattr(Style, setting["style"])
+            print(color, back, style, f"""
+
+            _        _                  _             
+  __ _  ___| |_     | |_ _ __ __ _  ___| | _____ _ __ 
+ / _` |/ __| __|____| __| '__/ _` |/ __| |/ / _ \ '__|
+| (_| | (__| ||_____| |_| | | (_| | (__|   <  __/ |   
+ \__,_|\___|\__|     \__|_|  \__,_|\___|_|\_\___|_|   
+
+Operating System: {self.user_os}  
+Path: {self.script}  
+Time: {date_time}  \n\n\n\n                    
+""")
+            f.close()
         print(f"Hello, {name}, it is currently {date_time}.")
+        print(f"Here are your current activities: {list_strip}.")
+        print("Do help or man to learn more about act-tracker!")
 
     def help(self):
         print("""Welcome to the activity tracker!\nCommands:\nchange_name: changes your name
 add_activity: adds an activity.\ndelete_all: deletes ALL activities\nlist_all: lists all current activities\ndelete_one: Delete one specified activity\ntime: tells the time\nset_color: change color of terminal
- """)
+set_back: changes the background color of text\nset_style: set the style of the text
+clear_on_start: toggle clearing the screen on startup.""")
